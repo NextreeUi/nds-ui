56801c57ec22e81d2ace8976a2768ee965a5c89c
NextreeUi <nextree@nextree.io>
2023-05-26 17:25:36 +0900

    dist

--- dist/assets/SelectPage-ed926768.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-4e960a9c.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("p",{className:"size24",children:"Select 페이지입니다."})});export{t as default};
---
+++ dist/assets/Slide-b8dead8a.js
@@ -0,0 +1,19 @@
+import{r,j as t,B as e}from"./index-e11ed23f.js";import{R as s,j as i}from"./index-50dcb135.js";const c=()=>{const[a,n]=r.useState(!1),o=()=>{n(!a)};return t.jsx(t.Fragment,{children:t.jsxs("div",{className:"layout-all",children:[t.jsxs("div",{className:"layout-title",children:[t.jsx("h1",{children:"Slide"}),t.jsx("p",{className:"description",children:"클릭 인터렉션이 있는 Checkbox 컴포넌트입니다."})]}),t.jsxs("div",{className:"layout-small",children:[t.jsxs("div",{className:"layout-title",children:[t.jsx("h2",{children:"Basic Slide"}),t.jsx("p",{className:"description",children:"variant 속성으로 버튼의 종류를 설정할 수 있습니다."})]}),t.jsxs("div",{className:"layout-example",children:[t.jsx(e,{children:" Noraml "}),t.jsx(e,{variant:"outlined",children:" Outlined  "}),t.jsx(e,{variant:"contained",children:" Contained "})]}),t.jsxs("div",{className:"codeMirror",children:[a?t.jsx(s,{theme:"dark",extensions:[i({jsx:!0})],value:`import * as React from 'react';
+import { Button } from 'nds-ui/component';
+import 'nds-ui/css/style.scss';
+
+const Example = () => {
+  return (
+    <div className='flex gap10'>
+      <Button> Noraml </Button>
+      <Button variant='outlined'> Outlined  </Button>
+      <Button variant='contained'> Contained </Button>
+    </div>
+  )
+}
+
+export default Example;`}):t.jsx(s,{theme:"dark",extensions:[i({jsx:!0})],value:`<>
+<Button> Noraml </Button>
+<Button variant='outlined'> Outlined  </Button>
+<Button variant='contained'> Contained </Button>
+</>`}),t.jsx(e,{square:!0,radius:"round",variant:"outlined",size:"extraSmall",hoverBg:"white",hoverColor:"black",theme:"white",onClick:o,children:t.jsx("i",{className:"i-code w14 h14"})})]})]})]})})};export{c as default};
